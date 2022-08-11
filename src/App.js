99fd47f71912b18dbdb7ab871cb255363fcc8d00
Tom Davidson <thomas.davidson@ibm.com>
2022-08-11 17:18:47 +0100

    adding first wind symbol

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import './App.css';
-import Map from './map'
+import Map from './Map'
 
 
 function App() {
 
+  const [hasSeaOverlay, setHasSeaOverlay] = React.useState();
+
+  const handleSeaOverlayClick = () => {
+    const current = hasSeaOverlay;
+    setHasSeaOverlay(!current)
+  };
+
   return (
     <div className="App">
-      <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}> 
+      <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', margin: '1rem 0'}}> 
         <p style={{color: "black", width: '17%', textAlign: 'justify'}}>Time: </p>
+        <button onClick={handleSeaOverlayClick}>Toggle Sea Overlay</button>
       </div>
       <div style={{height: "80vh"}}>
         <Map
           centerPoint={[50.7614912, -1.3812069]}
+          hasSeaOverlay={hasSeaOverlay}
         />
-  
+
       </div>
     </div>
   );
